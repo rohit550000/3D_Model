1b5407fcc35619a0ed101fe244bb70f8760ada23
Rohit kanojiya <123115001+rohit550000@users.noreply.github.com>
2023-02-12 00:12:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const btnOpen=document.getElementById("btn-open");
+const btnClose=document.getElementById("btn-close");
+const backChang=document.querySelector(".backgroundChang");
+
+
+
+btnOpen.addEventListener("click",()=>{
+    
+    backChang.style.display="initial";
+
+});
+
+
+btnClose.addEventListener("click",()=>{
+  
+    backChang.style.display="none";
+
+});
+
+
+
+
+
+// btnOpen.addEventListener("click",()=>{
+   
+//     backChang.classList.add("backchangg");
+
+// });
+
+
+// btnClose.addEventListener("click",()=>{
+  
+//     backChang.classList.remove("backchangg");
+
+// });
+
+
+
+
